7b137c940a171335f7c206d74a22920fdd64f542
Santiago Martín Agra <santiagomartinagra@gmail.com>
2022-04-14 18:13:57 +0200

    chore: initial commit

---
+++ test/unit/pages/index.spec.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+
+import Home from '../../../src/pages';
+
+describe('Home', () => {
+	it('welcomes the user', async () => {
+		const { getByText } = render(<Home />);
+
+		expect(getByText('Welcome to')).toBeInTheDocument();
+	});
+});
