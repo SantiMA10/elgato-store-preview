5e44cb191fbe859d4f063964ae9e56ed138b8080
Santiago Martín Agra <santiagomartinagra@gmail.com>
2022-04-14 18:25:07 +0200

    chore: add jest-chat-reporter

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,20 @@ const customJestConfig = {
 	// if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
 	moduleDirectories: ['node_modules', '<rootDir>/'],
 	testEnvironment: 'jest-environment-jsdom',
+	reporters: [
+		'default',
+		[
+			'@santima10/jest-chat-reporter',
+			{
+				channels: ['SantiMA10'],
+				username: process.env.TWITCH_USERNAME,
+				password: process.env.TWITCH_PASSWORD,
+				messagesOnWatchMode: false,
+				onlyCI: true,
+				useAnnounce: true,
+			},
+		],
+	],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
